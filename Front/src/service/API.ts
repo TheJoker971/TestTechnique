c734911f34e2bd860f49b081d3fa57129b5db7fb
Joackim <jojo.blsegor@gmail.com>
2024-12-04 10:51:07 +0100

    Remove useless

--- Front/src/service/API.ts
+++ Front/src/service/API.ts
@@ -1,9 +1,6 @@
-import { config } from "dotenv";
-config();
-
 export default class API {
 
     static getUrl() {
-        return process.env.API_URL;
+        return "http:/localhost:8080";
     }
 }
